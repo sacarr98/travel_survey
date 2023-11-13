f1ec2164f96ae1525652bd22cdb01b73ad5e45e1
sacarr98 <>
2023-11-13 12:46:47 +0000

    create function to write distance travelled data to worksheet and ensure data is in the correct format

--- run.py
+++ run.py
@@ -19,10 +19,59 @@ data = distance.get_all_values()
 
 def get_distance_data():
     """
-    Get distance figures input from the user.
+    Get distance travelled data input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be one value.
+    The loop will repeatedly request data, until it is valid.
     """
-    distance_data = input("How many miles do you travel to work everyday?\n")
+    while True:
+        print("Please enter your distance to work each day to the nearest mile.")
+
+        data_str = input("How many miles do you travel to work each day?\n")
+
+        distance_data = data_str.split(",")
+
+        if validate_data(distance_data):
+            print("Data is valid!")
+            break
 
     return distance_data
 
 
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if more than one value is entered.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 1:
+            raise ValueError(
+                f"Please only enter one value, you provided {len(values)}"
+            )
+        if isinstance(values, int) != True:
+            raise ValueError(
+                "Please enter your distance to the nearest whole mile"
+            )
+        
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+def update_distance_worksheet(data):
+    """
+    Update distance worksheet, add new row with the list data provided
+    """
+    print("Updating distance worksheet...\n")
+    sales_worksheet = SHEET.worksheet("distance")
+    sales_worksheet.append_row(data)
+    print("Distance worksheet updated successfully.\n")
+
+
+data = get_distance_data()
+distance_data = [int(num) for num in data]
+update_distance_worksheet(distance_data)
