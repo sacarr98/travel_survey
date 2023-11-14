db0f6df8486078cdc3cfdbe0faff4ca067e0c9d3
sacarr98 <>
2023-11-14 07:52:22 +0000

    add functin to get data about methods of transport to work and add data to spreadsheet

--- run.py
+++ run.py
@@ -14,7 +14,11 @@ SHEET = GSPREAD_CLIENT.open('travel_survey')
 
 distance = SHEET.worksheet('distance')
 
-data = distance.get_all_values()
+d_data = distance.get_all_values()
+
+transport = SHEET.worksheet('transport')
+
+t_data = transport.get_all_values()
 
 
 def get_distance_data():
@@ -49,12 +53,7 @@ def validate_data(values):
         if len(values) != 1:
             raise ValueError(
                 f"Please only enter one value, you provided {len(values)}"
-            )
-        if isinstance(values, int) != True:
-            raise ValueError(
-                "Please enter your distance to the nearest whole mile"
-            )
-        
+            )       
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
@@ -62,22 +61,50 @@ def validate_data(values):
     return True
 
 
-def update_distance_worksheet(data):
+def update_distance_worksheet(d_data):
     """
     Update distance worksheet, add new row with the list data provided
     """
     print("Updating distance worksheet...\n")
-    sales_worksheet = SHEET.worksheet("distance")
-    sales_worksheet.append_row(data)
+    distance_worksheet = SHEET.worksheet("distance")
+    distance_worksheet.append_row(d_data)
     print("Distance worksheet updated successfully.\n")
 
-def main():
+
+def get_transport_data():
+    """
+    Asks questions about method of travel to work, adds values to a list.
+    """
+    walking_str = input("How often do you walk to work each week?\n")
+
+    cycling_str = input("How often do you cycle to work each wwek?\n")
+
+    transport_data = [walking_str, cycling_str]
+
+    return transport_data
+
+
+def update_transport_worksheet(t_data):
+    """
+    Update distance worksheet, add new row with the list data provided
+    """
+    print("Updating transport worksheet...\n")
+    transport_worksheet = SHEET.worksheet("transport")
+    transport_worksheet.append_row(t_data)
+    print("Transport worksheet updated successfully.\n")
+
+
+def main(): 
     """
     Run all program functions
     """
-    data = get_distance_data()
-    distance_data = [int(num) for num in data]
+    d_data = get_distance_data()
+    distance_data = [int(num) for num in d_data]
     update_distance_worksheet(distance_data)
+    t_data = get_transport_data()
+    transport_data = [int(num) for num in t_data]
+    update_transport_worksheet(transport_data)
+
 
 print("Thank you for taking the time to complete our survey")
 main()
